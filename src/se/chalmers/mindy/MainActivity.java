12450075453a7746899d115e4171947774d56745
Viktor Åkerskog <akerskuuug@gmail.com>
2014-02-21 11:05:07 +0100

    Added a Navigation Drawer to MainActivity. Its functionality is stupid atm, will hopefully be less stupid later today.

--- src/se/chalmers/mindy/MainActivity.java
+++ src/se/chalmers/mindy/MainActivity.java
@@ -1,20 +1,73 @@
 package se.chalmers.mindy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerList;
+	private CharSequence mTitle;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true; 
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		String[] temp = { "Hem", "…vningar" };
+		// Set the adapter for the list view
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, temp));
+		// Set the list's click listener
+		mDrawerList.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// TODO Auto-generated method stub
+
+				selectItem(position);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {
+		// Create a new fragment and specify the planet to show based on
+		// position
+
+		/*
+		 * Fragment fragment = new PlanetFragment(); Bundle args = new Bundle();
+		 * args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+		 * fragment.setArguments(args);
+		 * 
+		 * // Insert the fragment by replacing any existing fragment
+		 * FragmentManager fragmentManager = getFragmentManager();
+		 * fragmentManager.beginTransaction() .replace(R.id.content_frame,
+		 * fragment) .commit();
+		 */
+		// Highlight the selected item, update the title, and close the drawer
+		mDrawerList.setItemChecked(position, true);
+		setTitle("Success");
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
 }
