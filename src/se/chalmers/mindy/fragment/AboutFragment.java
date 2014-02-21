3da1ca0cea2d3a910e256800cfd00ab909e444f5
pewhl <paula.eriksson@gmail.com>
2014-02-21 18:44:15 +0100

    Fixed conflicts

---
+++ src/se/chalmers/mindy/fragment/AboutFragment.java
@@ -0,0 +1,24 @@
+package se.chalmers.mindy.fragment;
+
+import se.chalmers.mindy.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class AboutFragment extends Fragment {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_about, null);
+		TextView tViewAbout = (TextView) view.findViewById(R.id.about);
+		TextView tViewAboutVersion = (TextView) view.findViewById(R.id.about_version);
+		TextView tViewAboutText = (TextView) view.findViewById(R.id.about_text);
+		TextView tViewAboutDevelopers = (TextView) view.findViewById(R.id.about_developers);
+		return view;
+	}
+
+
+
+}
