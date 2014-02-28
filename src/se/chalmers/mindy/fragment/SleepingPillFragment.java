570dd9db6427fd1f6c307aa2f5a6bbdf8e911c5a
Henrik Andersson <heenrik@hotmail.com>
2014-02-28 15:07:26 +0100

    Added sleeping pill fragment and added soundfile

---
+++ src/se/chalmers/mindy/fragment/SleepingPillFragment.java
@@ -0,0 +1,23 @@
+package se.chalmers.mindy.fragment;
+
+import se.chalmers.mindy.R;
+import android.app.Fragment;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SleepingPillFragment extends Fragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_sleepingpill, null);
+		
+		MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(), R.raw.sample_soundfile);
+		mediaPlayer.start();
+		
+		return view;
+	}
+}
+
