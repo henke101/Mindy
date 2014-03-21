d86d313b9b2d8e6d835fd76cfad7f335e3e119ec
Viktor Åkerskog <akerskuuug@gmail.com>
2014-03-21 15:31:25 +0100

    Implemented much of IndexFragment
    Implemented much of the IndexFragment (start screen), needs more
    exercise types.
    Also added new list item background, transparent navigation bar

--- src/se/chalmers/mindy/fragment/IndexFragment.java
+++ src/se/chalmers/mindy/fragment/IndexFragment.java
@@ -1,25 +1,35 @@
 package se.chalmers.mindy.fragment;
 
 import se.chalmers.mindy.R;
+import se.chalmers.mindy.core.IndexAdapter;
+import se.chalmers.mindy.core.IndexItem;
 import se.chalmers.mindy.core.MainActivity;
+import se.chalmers.mindy.core.SoundIndexItem;
 import se.chalmers.mindy.util.Tools;
 import android.app.Activity;
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class IndexFragment extends ListFragment {
+public class IndexFragment extends Fragment implements OnScrollListener {
 	MainActivity mActivity;
 	SharedPreferences sharedPrefs;
 	Editor editor;
+
+	private ListView mListView;
+	public View mListHeader;
 
 	@Override
 	public void onAttach(final Activity activity) {
@@ -32,24 +42,60 @@ public class IndexFragment extends ListFragment {
 	}
 
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		ListView listView = getListView();
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
 
-		View headerView = mActivity.getLayoutInflater().inflate(R.layout.list_header, null);
+		View root = inflater.inflate(R.layout.fragment_index, null);
+
+		mListView = (ListView) root.findViewById(R.id.index_list);
+		mListView.setDividerHeight(0);
+		mListView.setBackgroundColor(Color.rgb(226, 226, 226));
+		mListView.setHeaderDividersEnabled(true);
+
+		final View headerView = mActivity.getLayoutInflater().inflate(R.layout.list_header, null);
 
 		ImageView imageView = (ImageView) headerView.findViewById(R.id.header_background);
 		Tools.setTwoStepBitmapBackground(mActivity, R.drawable.fluff, imageView);
 
 		TextView titleView = (TextView) headerView.findViewById(R.id.header_title);
-		titleView.setText(R.string.app_name);
-		titleView.setTypeface(Typeface.createFromAsset(mActivity.getAssets(), "fonts/roboto_light.ttf"));
+		titleView.setText(R.string.title_fragment_index);
+		titleView.setTypeface(Typeface.createFromAsset(mActivity.getAssets(), "fonts/roboto_thin.ttf"));
 
-		listView.addHeaderView(headerView);
-		/** Setting the list adapter for the ListFragment */
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(mActivity.getLayoutInflater().getContext(), android.R.layout.simple_list_item_1, mActivity.getResources().getStringArray(
-				R.array.section_names));
-		setListAdapter(adapter);
+		mListHeader = headerView;
+		mListView.addHeaderView(headerView);
+
+		// Dummy items
+		IndexItem[] items = {
+				new SoundIndexItem(mActivity, "Sšmnpiller", "Perfekt fšr dig som har svŒrt att sova pŒ kvŠllarna, prova švningen liggandes i sŠngen nŠr du gŒtt och lagt dig", R.raw.sample_soundfile),
+				new SoundIndexItem(mActivity, "Kroppsscanning",
+						"Kroppscanning kan vara vŠldigt behagligt och dŠrfšr somnar mŒnga av švningen. Och mŒnga beskriver hur de sover djupare, Šven efter mŒnga Œrs sšmnproblem.",
+						R.raw.sample_soundfile),
+				new SoundIndexItem(mActivity, "Sšmnpiller", "Perfekt fšr dig som har svŒrt att sova pŒ kvŠllarna, prova švningen liggandes i sŠngen nŠr du gŒtt och lagt dig", R.raw.sample_soundfile),
+				new SoundIndexItem(mActivity, "Sšmnpiller", "Perfekt fšr dig som har svŒrt att sova pŒ kvŠllarna, prova švningen liggandes i sŠngen nŠr du gŒtt och lagt dig", R.raw.sample_soundfile) };
+		IndexAdapter adapter = new IndexAdapter(mActivity, items);
+
+		mListView.setAdapter(adapter);
+
+		return root;
 
 	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		mListView.setOnScrollListener(this);
+	}
+
+	@Override
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		if (visibleItemCount > firstVisibleItem) {
+			mActivity.setNavigationBarBackgroundTransparency(view, mListHeader.getHeight(), firstVisibleItem);
+		}
+	}
+
+	@Override
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		// Do nothing
+	}
+
 }
