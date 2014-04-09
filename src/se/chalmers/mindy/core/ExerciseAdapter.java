bc04285b8c831c815b158e97c9df7359b2111aea
Viktor Åkerskog <akerskuuug@gmail.com>
2014-04-09 09:18:32 +0200

    Font unification
    Unified fonts.

--- src/se/chalmers/mindy/core/ExerciseAdapter.java
+++ src/se/chalmers/mindy/core/ExerciseAdapter.java
@@ -10,27 +10,29 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class ExerciseAdapter extends ArrayAdapter<ExerciseItem>{
-	
+public class ExerciseAdapter extends ArrayAdapter<ExerciseItem> {
+
 	Context context;
 	int layoutResourceId;
 	ExerciseItem data[] = null;
-	
+	private Typeface robotoThin;
+	private Typeface robotoLight;
+
 	public ExerciseAdapter(final Context context, final int layoutResourceId, final ExerciseItem[] data) {
 		super(context, layoutResourceId, data);
 		this.layoutResourceId = layoutResourceId;
 		this.context = context;
 		this.data = data;
+		robotoThin = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_thin.ttf");
+		robotoLight = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_light.ttf");
 	}
-	
+
 	@Override
 	public View getView(final int position, final View convertView, final ViewGroup parent) {
 		View row = convertView;
 		ExerciseItemHolder holder = null;
-		Typeface robotoConLight = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_condensed_light.ttf");
-		Typeface robotoLight = Typeface.createFromAsset(context.getAssets(), "fonts/roboto_light.ttf");
-		
-		if(row == null){
+
+		if (row == null) {
 			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
 			row = inflater.inflate(layoutResourceId, parent, false);
 
@@ -38,21 +40,21 @@ public class ExerciseAdapter extends ArrayAdapter<ExerciseItem>{
 
 			holder.exercise = (TextView) row.findViewById(R.id.exercise_label);
 			holder.exercise.setText(data[position].getName());
-			holder.exercise.setTypeface(robotoConLight);
-			
+			holder.exercise.setTypeface(robotoThin);
+
 			holder.description = (TextView) row.findViewById(R.id.description_label);
 			holder.description.setText(data[position].getDescription());
 			holder.description.setTypeface(robotoLight);
 
 		}
-		
+
 		holder = (ExerciseItemHolder) row.getTag();
-		
+
 		return row;
 	}
-	
+
 	static class ExerciseItemHolder {
-		
+
 		TextView exercise, description;
 	}
 }
